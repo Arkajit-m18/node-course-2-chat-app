6334026e7c100c34ebfba29b8ac6fb243123c6a4
Arkajit Mondal <arkajit.18@gmail.com>
2018-10-09 00:36:38 +0530

    Add css for chat page

--- server/server.js
+++ server/server.js
@@ -16,22 +16,6 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected.');
 
-    // socket.emit('newEmail', {
-    //     from: 'mike@example.com',
-    //     text: 'Hey! What is going on?',
-    //     createdAt: 123
-    // });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('Create email: ', newEmail);
-    // });
-
-    // socket.emit('newMessage', {
-    //     from: 'june@example.com',
-    //     text: 'Let\'s meet up at 6.',
-    //     createdAt: 1234
-    // });
-
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
     
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined'));
@@ -40,13 +24,7 @@ io.on('connection', (socket) => {
         console.log('Create message: ', message);
         
         io.emit('newMessage', generateMessage(message.from, message.text));
-        callback('This is from the server.');
-        
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
+        callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
